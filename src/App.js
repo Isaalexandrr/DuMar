d100dc21f1bc743e12fce74757a96506c29d318d
GabrielaGouveia <gabii77leone@gmail.com>
2023-10-29 21:31:54 -0300

    definindo cor da tela

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { Button } from 'react-bootstrap';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const estiloDeFundo = {
+    backgroundColor: '#538AAD', // Substitua pela cor desejada
+    color: '#ffffff', // Cor do texto
+    minHeight: '100vh', // Para garantir que o fundo se estenda até o final da página
+    
+  
+  };
+  
+
   return (
-    <div>
-      <h1>
-        Example heading
-        <p bg="secondary" as={Button}>
-          New
-        </p>
-      </h1>
+    <div style={estiloDeFundo}>
+      <p className="font-custom">DuMar</p>
+  
+    
     </div>
+    
   );
 }
+
+
 
 export default App;
