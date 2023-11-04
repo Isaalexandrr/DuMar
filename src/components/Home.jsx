fb613089f6abb27d397e20224c1a872baca9fd69
isaalexandrr <isabelaalexandre8@gmail.com>
2023-11-03 21:05:17 -0300

    alteracao

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './home.module.css';
+import styles from './home.module.css';
+import logo2 from './imagens/logo2.png';
+import peixe from './imagens/peixe.png';
+import menu from './imagens/menu.png';
 
 function Home() {
   return (
     <div className="serch-box">
-          <div className="serch-btn">
-            <input type="text" className="serch-txt" placeholder="Pesquisar" />
-        </div>
+      <div className="serch-btn">
+        <input type="text" className="serch-txt" placeholder="Pesquisar" />
+      </div>
 
-      <div className="img-logo2">
-          <img src="./imagens/logo2.png" className="img-logo2" alt="DUMAR" />
+      <div className={styles['img-logo2']}>
+      <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
       </div>
       <div className="titulo">
-       <h1 className="text-primary">Consumo Consciente</h1>
+        <h1 className="text-primary">Consumo Consciente</h1>
       </div>
       <div className="subtitulo">
-        <h2> Você sabia que 70% dos <br /> resíduos do mar brasileiro <br /> são plástico?</h2>
+        <h2> Você sabia que 70% dos resíduos do mar brasileiro são plástico?</h2>
       </div>
-      <div className="img-peixe">
-          <img src="/imagens/peixe.png" className="img-peixe" alt="peixinhos" />
+      <div className={styles['img-peixe']}>
+       <img src={peixe} className={styles['img-peixe']} alt="peixinhos" />
       </div>
-
       <div className="img-menu">
-          <img src="menu.png" className="img-menu" alt="menu" />
+        <img src={menu} className="img-menu" alt="menu" />
       </div>
     </div>
   );
 }
 
 export default Home;
+
