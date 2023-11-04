5dfca187414b40fa23cea6683c8111c53cdcaa22
GabrielaGouveia <gabii77leone@gmail.com>
2023-11-03 23:43:06 -0300

    atualiza home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './home.module.css';
 import logo2 from './imagens/logo2.png';
 import peixe from './imagens/peixe.png';
-import menu from './imagens/menu.png';
+
 
 function Home() {
   return (
@@ -13,8 +13,12 @@ function Home() {
       </div>
 
       <div className={styles['img-logo2']}>
-      <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
+       <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
       </div>
+      <div className="container mt-5">
+        <button className={`btn btn-sm ${styles.botaoEscuro}`}>Entrar</button>
+     </div>
+
       <div className="titulo">
         <h1 className="text-primary">Consumo Consciente</h1>
       </div>
@@ -24,10 +28,9 @@ function Home() {
       <div className={styles['img-peixe']}>
        <img src={peixe} className={styles['img-peixe']} alt="peixinhos" />
       </div>
-      <div className="img-menu">
-        <img src={menu} className="img-menu" alt="menu" />
-      </div>
+      
     </div>
+    
   );
 }
 
