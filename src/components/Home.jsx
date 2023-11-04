c20a3e694266ffdd853d3e94b8238e906df376b8
isaalexandrr <isabelaalexandre8@gmail.com>
2023-11-03 23:32:38 -0300

    atulizacao home imagens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './home.module.css';
 import logo2 from './imagens/logo2.png';
 import peixe from './imagens/peixe.png';
-import menu from './imagens/menu.png';
+import onda1 from './imagens/onda1.png';
+import aguaviva from './imagens/aguaviva.png';
+import alga from './imagens/alga.png';
 
 function Home() {
   return (
@@ -19,14 +21,26 @@ function Home() {
         <h1 className="text-primary">Consumo Consciente</h1>
       </div>
       <div className="subtitulo">
-        <h2> Você sabia que 70% dos resíduos do mar brasileiro são plástico?</h2>
+        <h2> Você sabia que 70% dos <br /> resíduos do mar brasileiro <br /> são plástico?</h2>
       </div>
+
       <div className={styles['img-peixe']}>
-       <img src={peixe} className={styles['img-peixe']} alt="peixinhos" />
+       <img src={peixe} className={styles['img-peixe']} alt="Peixinhos" />
       </div>
-      <div className="img-menu">
-        <img src={menu} className="img-menu" alt="menu" />
+        <div className={styles['img-alga']}>
+       <img src={alga} className={styles['img-alga']} alt="Alga" />
       </div>
+
+      <div className={styles['img-onda1']}>
+       <img src={onda1} className={styles['img-onda1']} alt="Onda" />
+      </div>
+
+      <div className={styles['img-aguaviva']}>
+       <img src={aguaviva} className={styles['img-aguaviva']} alt="Agua viva" />
+      </div>
+
+     
+
     </div>
   );
 }
