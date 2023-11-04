b79193523d71e86d2069606152b094af5976ff19
Andreza Caldeira <Andrezacalderamorais@hotmail.com>
2023-11-04 00:03:38 -0300

    atualização tela inicial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,22 +6,24 @@ import peixe from './imagens/peixe.png';
 import onda1 from './imagens/onda1.png';
 import aguaviva from './imagens/aguaviva.png';
 import alga from './imagens/alga.png';
+import menu from './imagens/menu.png';
+import onda2 from './imagens/onda2.png';
+import peixe2 from './imagens/peixe2.png';
 
 function Home() {
   return (
-    <div className="serch-box">
+    <>
       <div className="serch-btn">
         <input type="text" className="serch-txt" placeholder="Pesquisar" />
       </div>
-
       <div className={styles['img-logo2']}>
       <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
       </div>
-      <div className="titulo">
-        <h1 className="text-primary">Consumo Consciente</h1>
+      <div className={styles['titulo']}>
+        <p>Consumo Consciente</p>
       </div>
-      <div className="subtitulo">
-        <h2> Você sabia que 70% dos <br /> resíduos do mar brasileiro <br /> são plástico?</h2>
+      <div className={styles['subtitulo']}>
+        <p> Você sabia que 70% dos resíduos do mar brasileiro são plástico?</p>
       </div>
 
       <div className={styles['img-peixe']}>
@@ -30,7 +32,15 @@ function Home() {
         <div className={styles['img-alga']}>
        <img src={alga} className={styles['img-alga']} alt="Alga" />
       </div>
-
+      <div>
+      <img src={peixe2} className="peixe2" alt="icone" />
+      </div>
+      <div>
+        <img src={alga} className="alga" alt="icone" />
+      </div>
+      <div>
+        <img src={onda2} className="onda2" alt="icone" />
+      </div>
       <div className={styles['img-onda1']}>
        <img src={onda1} className={styles['img-onda1']} alt="Onda" />
       </div>
@@ -38,10 +48,7 @@ function Home() {
       <div className={styles['img-aguaviva']}>
        <img src={aguaviva} className={styles['img-aguaviva']} alt="Agua viva" />
       </div>
-
-     
-
-    </div>
+      </>
   );
 }
 
