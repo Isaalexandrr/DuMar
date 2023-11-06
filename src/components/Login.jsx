634e10355ae8c19c0256fa1c0ae8ecfad1718be5
Alanes <alanesGsilva@gmail.com>
2023-11-05 21:09:30 -0300

    commit login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ClassNames } from '@emotion/react';
 
 
 function Login() {
+    
     return (
-        <div class="container mt-4">
-        <div class="row justify-content-left">
-            <div class="col-md-10 mx-auto col-lg-5">
-                <h1 class="text-center mb-4">Bem-Vindo(a).Acesse sua conta!</h1>
-                <form class="p-4 p-md-5 border rounded-4 bg-#538AAD">
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="inputEmail" placeholder="E-mail"/>
+      
+        <div className="container mt-10">
+            <img src="onda5.png" class="w-15"></img>
+        <div className="row justify-content-left">
+            <div className="col-md-10 mx-auto col-lg-5">
+                <h1 className="text align:center mb-5 justify-content-left"></h1>
+                <h1 style={{color:'#a8f2fe'}}> Bem-Vindo(a). Acesse  sua conta! </h1>
+                <form className="p-4 p-md-5  rounded-4 bg-#538AAD">
+
+                    <div className="form-floating mb-3">
+                        <input type="email" className="form-control" id="inputEmail" placeholder="E-mail"/>
                         <label for="inputEmail">E-mail</label>
                     </div>
-                    <div class="form-floating mb-4">
-                        <input type="password" class="form-control" id="inputPassword" placeholder="Senha"/>
+                    <div className="form-floating mb-4">
+                        <input type="password" className="form-control" id="inputPassword" placeholder="Senha"/>
                         <label for="inputPassword">Senha</label>
                     </div>
-                    <div class="checkbox mb-4">
+                    <div className="checkbox mb-4">
                         <input class="form-check-input" type="checkbox" value="Lembrar-me" id="rememberMe"/>
                         <label class="form-check-label" for="rememberMe">Lembrar-me</label>
                     </div>
-                    <button class="w-100 btn btn-lg btn-success" type="submit">Entrar</button>
-                    <p style="color:blue">Ainda não tem uma conta? Inscreva-se</p>
+                    <button type="Submit" className="w-100 btn btn-lg btn-info">Entrar</button>
+                    <p style={{color:'#a8f2fe'}}>Ainda não tem uma conta? Inscreva-se</p>
                 </form>
             </div>
         </div>
