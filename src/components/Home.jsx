4f52619383bea3b796e8bb9240e81e881b078eda
Andreza Caldeira <Andrezacalderamorais@hotmail.com>
2023-11-06 12:47:12 -0300

    Alteração de imagens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './home.module.css';
 import logo2 from './imagens/logo2.png';
-import peixe from './imagens/peixe.png';
-import onda1 from './imagens/onda1.png';
-import aguaviva from './imagens/aguaviva.png';
 import menu from './imagens/menu.png';
-import onda2 from './imagens/onda2.png';
-import alga from './imagens/alga.png';
-import peixe2 from './imagens/peixe2.png';
+import lado_direito from './imagens/lado_direito.png';
+import lado_esquedo from './imagens/lado_esquerdo.png';
 
 function Home() {
   return (
     <>
       <div className="serch-btn">
         <input type="text" className="serch-txt" placeholder="Pesquisar" />
+      </div>
+      <div className={styles['img-menu']}>
+       <img src={menu} className={styles['menu']} alt="icones" />
       </div>
       <div className={styles['img-logo2']}>
       <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
@@ -25,28 +24,11 @@ function Home() {
       <div className={styles['subtitulo']}>
         <p> Você sabia que 70% dos resíduos do mar brasileiro são plástico?</p>
       </div>
-
-      <div className={styles['img-peixe']}>
-       <img src={peixe} className={styles['img-peixe']} alt="Peixinhos" />
+      <div className={styles['img-esquerdo']}>
+       <img src={lado_esquedo} className={styles['lado_esquerdo']} alt="icones" />
       </div>
-        <div className={styles['img-alga']}>
-       <img src={alga} className={styles['img-alga']} alt="Alga" />
-      </div>
-      <div>
-      <img src={peixe2} className="peixe2" alt="icone" />
-      </div>
-      <div>
-        <img src={alga} className="alga" alt="icone" />
-      </div>
-      <div>
-        <img src={onda2} className="onda2" alt="icone" />
-      </div>
-      <div className={styles['img-onda1']}>
-       <img src={onda1} className={styles['img-onda1']} alt="Onda" />
-      </div>
-
-      <div className={styles['img-aguaviva']}>
-       <img src={aguaviva} className={styles['img-aguaviva']} alt="Agua viva" />
+      <div className={styles['img-direito']}>
+       <img src={lado_direito} className={styles['lado_direito']} alt="icones" />
       </div>
       </>
   );
