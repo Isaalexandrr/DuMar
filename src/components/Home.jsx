c99ad7f9b0b4761b9c21ddf66733f6a349816bd5
Andreza Caldeira <Andrezacalderamorais@hotmail.com>
2023-11-09 15:18:19 -0300

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,11 @@ import styles from './home.module.css';
 import logo2 from './imagens/logo2.png';
 import userIcon from './imagens/login.png'; 
 import cartIcon from './imagens/carrinho.png'
-import peixe from './imagens/peixe.png';
-import onda1 from './imagens/onda1.png';
-import aguaviva from './imagens/aguaviva.png';
-import onda2 from './imagens/onda2.png';
-import alga from './imagens/alga.png';
-import peixe2 from './imagens/peixe2.png';
 import loupeIcon from './imagens/loupe.png';
+import menu from './imagens/menu.png';
+import lado_direito from './imagens/lado_direito.png';
+import lado_esquedo from './imagens/lado_esquerdo.png';
+
 
 function Home() {
   
@@ -37,43 +35,20 @@ function Home() {
           <img src={logo2} className={styles['img-logo2']} alt="DUMAR" />
         </div>
   
-        <div className="titulo">
-          <h1 className="text-primary">Consumo Consciente</h1>
-        </div>
-  
-        <div className="subtitulo">
-          <h2>
-            Você sabia que 70% dos <br /> resíduos do mar brasileiro <br /> são plástico?
-          </h2>
-        </div>
-  
-        <div className={styles['img-peixe']}>
-          <img src={peixe} className={styles['img-peixe']} alt="Peixinhos" />
-        </div>
-  
-        <div className={styles['img-alga']}>
-          <img src={alga} className={styles['img-alga']} alt="Alga" />
-        </div>
-  
-        <div>
-          <img src={peixe2} className="peixe2" alt="icone" />
-        </div>
-  
-        <div>
-          <img src={onda2} className="onda2" alt="icone" />
-        </div>
-  
-        <div className={styles['img-onda1']}>
-          <img src={onda1} className={styles['img-onda1']} alt="Onda" />
-        </div>
-  
-        <div className={styles['img-aguaviva']}>
-          <img src={aguaviva} className={styles['img-aguaviva']} alt="Agua viva" />
-        </div>
+        <div className={styles['titulo']}>
+        <p>DuMar para sua mesa!</p>
+      </div>
+      <div className={styles['subtitulo']}>
+        <p> Produtos fresquinho diariamente!</p>
+      </div>
+      <div className={styles['img-esquerdo']}>
+       <img src={lado_esquedo} className={styles['lado_esquerdo']} alt="icones" />
+      </div>
+      <div className={styles['img-direito']}>
+       <img src={lado_direito} className={styles['lado_direito']} alt="icones" />
+      </div>
       </div>
     );
-  
-  
 } 
 export default Home;
   
