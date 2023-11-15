f2f6cd42ae34c6e49aa947f2ec860fd3d8905d04
GabrielaGouveia <gabii77leone@gmail.com>
2023-11-14 22:32:52 -0300

    atualiza tipo de imagens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,6 +14,7 @@ import ondaestrela from './imagens/onda-estrela-2.png';
 
 
 
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
 
 function Home() {
@@ -48,13 +49,13 @@ function Home() {
   
           <div className={styles['pessoa-carro']}>
 
-          <button type="button" class="btn btn-secondary btn-sm" className={styles['entrar']}> 
+          <button type="button" class=" btn-secondary btn-sm" className={styles['entrar']}> 
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16" color='white'> 
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/></svg>
             <p>Entrar</p>
           </button>
   
-          <button type="button" class="btn btn-secondary btn-sm"  className={styles['carrinho']} >
+          <button type="button" class="btn-secondary btn-sm"  className={styles['carrinho']} >
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16" color='white'>
               <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/></svg>
           </button>
@@ -154,22 +155,22 @@ function Home() {
                 </div>
 
                 <div className={styles['grid-caixa']}>
-                    <img src="" alt="Salmão" />
-                    <h3>Salmão</h3>
+                    <img src="" alt="Camarão" />
+                    <h3>Camarão</h3>
                     <p>R$</p>
                     <input type="button" name="" id="" value="Comprar"/>
                 </div>
 
                 <div className={styles['grid-caixa']}>
-                    <img src="" alt="Salmão" />
-                    <h3>Salmão</h3>
+                    <img src="" alt="Tilapia" />
+                    <h3>Tilapia</h3>
                     <p>R$</p>
                     <input type="button" name="" id="" value="Comprar"/>
                 </div>
 
                 <div className={styles['grid-caixa']}>
-                    <img src="" alt="Salmão" />
-                    <h3>Salmão</h3>
+                    <img src="" alt="Lagosta" />
+                    <h3>Lagosta</h3>
                     <p>R$</p>
                     <input type="button" name="" id="" value="Comprar"/>
                 </div>
