bbe8032f0c614d111d06507938408fbbda92d031
isaalexandrr <isabelaalexandre8@gmail.com>
2023-11-23 20:24:01 -0300

    atulizacao

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import styles from "./Perfil.module.css";
+
+
+
+
+
+
+
+
+
+function Perfil(){
+    return(
+        <div>Perfil</div>
+    ); 
+
+}
+export default Perfil;
