bc78fe5185248343f9b1b33eb2c3d9f9f07c6230
gersonbarbosadev <gersononline42@gmail.com>
2023-11-23 04:14:16 -0300

    tela de cadastro responsiva

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './components/Home';
-import Login from './components/Login';
-import Products from './components/Products';
-import Cadastro from './components/Cadastro';
-=======
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Navbar from './components/Navbar';
 import { Consumo } from './pages/Consumo';
+import Cadastro from './pages/Cadastro';
 
->>>>>>> 9d01d7f4321732b080fbf5c126d939a0744b850e
 
 
 function App() {
@@ -26,13 +20,10 @@ function App() {
         </nav>
 
         <Routes>
+          <Route path="/Cadastro" element={<Cadastro />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Products" element={<Products />} /> 
-<<<<<<< HEAD
-          <Route path="/Cadastro" element={<Cadastro />} />
-=======
           <Route path="/Consumo" element={<Consumo/>} />
->>>>>>> 9d01d7f4321732b080fbf5c126d939a0744b850e
           <Route path="/" element={<Home />} /> 
         </Routes>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
-
 import logo1 from "../imagens/logo1.png";
 import logo2 from "../imagens/logo2.png";
 import styles from "./Navbar.module.css";
-
-
 import { IoSearch, IoCartOutline, IoPersonSharp } from "react-icons/io5";
 
 function Navbar() {
