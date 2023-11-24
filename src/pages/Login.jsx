2f7b295b9bf6009aeb517532137fbf7849569045
Alanes Gomes <alanesGsilva@gmail.com>
2023-11-24 17:11:29 -0300

    commit login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,13 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './login.module.css';
 import ondaestrela3 from "../imagens/onda-estrela-3.png";
 import ondacoral from "../imagens/onda-coral.png";
+import {IoPersonCircle } from "react-icons/io5";
+import { IoLockClosed, IoEye, IoEyeOff  } from "react-icons/io5";
+
 
 function Login() {
     return (
-        <div className={styles.container} class="container mt-4">
+
+        <div className={styles.container} class= "container mt-4">
         <div class="row justify-content-center">
-        <div class="col-md-7 mx-auto col-lg-5">
-          <div class="text-center mb-4 justify-content-center">
+        <div class="col-md-10 mx-auto col-lg-5">
+          <div class="text-center mb-4">
                     
                     <h1 className={styles.welcome}>
                                   Bem-Vindo (a).
@@ -17,14 +21,23 @@ function Login() {
                                 Acesse sua conta!
                         </h1>
 
-                        <form class="p-5 p-md-6">
-                    <div class="form-floating mb-3 justify-content-center">
+                        <form class="p-4 p-md-5">
+                    <div class="form-floating mb-3">
                         <input className={styles.email} type="email"  placeholder="E-mail"/>
+
+                        {/* <div className={styles["icone"]} /> */}
+                        {/* <IoPersonCircle /> */}
                     </div>
 
-                    <div class= "form-group m-3 justify-content-center" >
+                    <div class= "form-floating mb-4 justify-content-center" >
                         <input className={styles.senha} type="password" name="" id="" placeholder="Senha"/>
-                        <i class= "BsPersonCircle"></i>
+                        {/* <div className={styles["icones"]} /> */}
+                        {/* <IoLockClosed /> */}
+
+                        {/* <button className="eyes">
+                        <IoEyeOff />
+                        <IoEye />
+                        </button> */}
                         <div className={styles.esqueceuSenha} type="text-end-right">
                             <small>
                                 <a href="">Esqueceu sua senha?</a>
@@ -44,9 +57,17 @@ function Login() {
                             <input className={styles.inscreva} type="button" value="Inscreva-se!"/>
                             
                             </div>
+                              
                             </form>
                             </div>
-                            
+                            <div class="col-2 d-none d-lg-block d-xl-block d-xxl-block m-0 p-0">
+                        <img
+                            src={ondacoral}
+                            className={styles.ondacoral}
+                            style= {{ width: "100%", height: "auto" }}
+                            alt="ondacoral" >
+                              </img>
+                              </div>
                             </div>
                             
                         </div>
