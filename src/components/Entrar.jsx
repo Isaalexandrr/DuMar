c16ca01384c2ad9b33f5c8853c4c2172e1ca7ff1
isaalexandrr <isabelaalexandre8@gmail.com>
2023-11-30 20:09:15 -0300

    alteracao

--- src/components/Entrar.jsx
+++ src/components/Entrar.jsx
@@ -1,11 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown";
 import styles from "./Entrar.module.css";
-import BotaoAdicionar from "../components/Botao/BotaoAdicionar";
-import BotaoApagarItem from "../components/Botao/BotaoApagarItem";
 import {IoCartSharp} from "react-icons/io5";
-
 import { IoPersonSharp } from "react-icons/io5";
 
 function Entrar() {
