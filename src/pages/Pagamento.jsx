55e801e5c31aa9367a614668f3059c08b685a79c
GabrielaGouveia <gabii77leone@gmail.com>
2023-11-30 20:00:00 -0300

    validação

--- src/pages/Pagamento.jsx
+++ src/pages/Pagamento.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import  {useState}from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './Pagamento.module.css';
 import card from '../imagens/card_img.png';
+import { alertClasses } from "@mui/material";
 
 
 
 function Pagamento() {
+
+    const[nome, setNome]= useState('')
+    const[email, setEmail]= useState('')
+    const[endereco, setEndereco]= useState('')
+    const[cidade, setCidade]= useState('')
+    const[estado, setEstado]= useState('')
+    const[cep, setCep]= useState('')
+    const[pix, setPix]= useState('')
+    const[nomecartao, setNomeCartao]= useState('')
+    const[numerocartao, setNumeroCartao]= useState('')
+    const[mesexpiracao, setMesExpiracao]= useState('')
+    const[anoexpiracao, setAnoExpiracao]= useState('')
+    const[cvv, setCvv]= useState('')
+    const[msg, setMsg]= useState('')
 
 
   return (
@@ -17,28 +32,46 @@ function Pagamento() {
               <h3 className={styles.title}>Endereço de Cobrança</h3>
               <div className={styles.inputBox}>
                 <span>Nome Completo:</span>
-                <input type="text"  placeholder="Seu Nome Completo"/>
+                <input type="text"  placeholder="Seu Nome Completo" value={nome} onChange={(e)=>
+                 
+                 setNome(e.target.value)
+                }/>
               </div>
               <div className={styles.inputBox}>
                 <span>Email:</span>
-                <input type="email" placeholder="Example@example.com" />
+                <input type="email" placeholder="Example@example.com"  value={email} onChange={(e)=>
+                 
+                 setEmail(e.target.value)
+                }/>
               </div>
               <div className={styles.inputBox}>
                 <span>Endereço:</span>
-                <input type="text" placeholder="Digite seu endereço completo" />
+                <input type="text" placeholder="Digite seu endereço completo" value={endereco} onChange={(e)=>
+                 
+                 setEndereco(e.target.value)
+                }/>
               </div>
               <div className={styles.inputBox}>
                 <span>Cidade:</span>
-                <input type="text" placeholder="Recife" />
+                 <input type="text" placeholder="Recife" value={cidade} onChange={(e)=>
+                 
+                 setCidade(e.target.value) 
+                 }/>
               </div>
               <div className={`${styles.flex} ${styles.addressFlex}`}>
                 <div className={styles.inputBox}>
                   <span>Estado:</span>
-                  <input type="text" placeholder="Pernambuco" />
+                  <input type="text" placeholder="Pernambuco"  value={estado} onChange={(e)=>
+                 
+                 setEstado(e.target.value) 
+                 }/>
                 </div>
                 <div className={styles.inputBox}>
                   <span>CEP:</span>
-                  <input type="text" placeholder="123 456 123 -88" />
+                  <input type="text" placeholder="123 456 123 -88" value={cep} onChange={(e)=>
+                 
+                 setCep(e.target.value) 
+                 }/> 
                 </div>
               </div>
             </div>
@@ -63,29 +96,47 @@ function Pagamento() {
               </div>
               <div className={styles.inputBox}>
                 <span>Chave PIX:</span>
-                <input type="text" placeholder="Exemplo: seuemail@provedor.com.br"/>
+                <input type="text" placeholder="Exemplo: seuemail@provedor.com.br" value={pix} onChange={(e)=>
+                 
+                 setPix(e.target.value) 
+                 }/> 
               </div>
 
               <div className={styles.inputBox}>
                 <span>Nome do cartão:</span>
-                <input type="text"  placeholder="Nome impresso no cartão"/>
+                <input type="text"  placeholder="Nome impresso no cartão" value={nomecartao} onChange={(e)=>
+                 
+                 setNomeCartao(e.target.value) 
+                 }/>
               </div>
               <div className={styles.inputBox}>
                 <span>Número do cartão de crédito :</span>
-                <input type="number" placeholder="1111-2222-3333-4444" />
+                <input type="number" placeholder="1111-2222-3333-4444" value={numerocartao} onChange={(e)=>
+                 
+                 setNumeroCartao(e.target.value) 
+                 }/>
               </div>
               <div className={styles.inputBox}>
                 <span>Mês de expiração:</span>
-                <input type="text" placeholder="Janeiro" />
+                <input type="text" placeholder="Janeiro" value={mesexpiracao} onChange={(e)=>
+                 
+                 setMesExpiracao(e.target.value) 
+                 }/>
               </div>
               <div className={`${styles.flex} ${styles.paymentFlex}`}>
                 <div className={styles.inputBox}>
                   <span>Ano de expiração:</span>
-                  <input type="number" placeholder={2022} />
+                  <input type="number" placeholder={2022} value={anoexpiracao} onChange={(e)=>
+                 
+                 setAnoExpiracao(e.target.value) 
+                 }/>
                 </div>
                 <div className={styles.inputBox}>
                   <span>CVV :</span>
-                  <input type="text" placeholder={1234} />
+                  <input type="text" placeholder={1234} value={cvv} onChange={(e)=>
+                 
+                 setCvv(e.target.value) 
+                 }/> 
                 </div>
               </div>
             </div>
@@ -94,10 +145,32 @@ function Pagamento() {
             className={styles.submitBtn}
             type="submit"
             value="Finalizar pedido"
+            onClick={()=>{
+                if (nome==''){
+                    
+                    setMsg('Preencha seu Nome Completo')
+                    return false
+    
+                }
+
+
+            }}
           />
         </form>
+        <div className='row-mt-3'>
+            <div className='col'>
+                <alert variant= 'warning'>
+                    {msg}
+
+                </alert>
+            </div>
+        </div>
+
+
       </div>
     </div>
+
+     
   );
 }
 
