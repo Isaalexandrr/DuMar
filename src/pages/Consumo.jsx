920512751b63fe4deb9952a8cfc4d658e687a264
GabrielaGouveia <gabii77leone@gmail.com>
2023-12-04 18:03:11 -0300

    .

--- src/pages/Consumo.jsx
+++ src/pages/Consumo.jsx
@@ -11,7 +11,7 @@ import limpandolixo from "../imagens/limpandolixo.png";
 import reciclavel from "../imagens/reciclavel.png";
 import proibidonadar from "../imagens/proibidonadar.png";
 import { useEffect } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+
 
 const Consumo = () => {
   useEffect(() => {
@@ -31,161 +31,122 @@ const Consumo = () => {
           <div className={styles["nav-bar"]}></div>
         </div>
 
-        <section>
+        <section className="container mt-5">
+        <div className="row">
+        <div className="col-12">
+         <h1 className={styles["consumo-c"]}>CONSUMO CONSCIENTE!</h1>
+         </div>
+         <div className="col-sm-12 col-md-4 mb-4">
           <div
-            className="row px-4 gap-5"
-            style={{
-              position: "absolute",
-              top: "220%",
-              transform: "translateY(-50%)",
-              width: "95%",
-              left: 200,
-            }}
-          > 
-           
-            <div
-              className="container-consumo"
-             
-            >
-              <h1 className={styles["consumo-c"]}>CONSUMO CONSCIENTE!</h1>
-            </div>
-            <div
-              className="col-md-3 col-sm-12 "
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.363)",
-                borderRadius: "15px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img className={styles["img-inter"]} src={icon} alt="" />
-              <div className={styles["texto-sobre"]}>O que?</div>
-              <p className={styles["consumo"]}>
-                Consumo consciente é fazer escolhas de compra considerando o
-                impacto ambiental e social dos produtos. Envolve preferir opções
-                sustentáveis e éticas para promover um estilo de vida mais
-                responsável.
-              </p>
-            </div>
-
-            <div
-              className="col-md-3 col-sm-12 "
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.363)",
-                borderRadius: "15px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img className={styles["img-inter"]} src={icon} alt="" />
-              <div className={styles["texto-sobre"]}>Como?</div>
-              <p className={styles["consumo"]}>
-                Pratique o consumo consciente ao optar por produtos duráveis e
-                sustentáveis. Reduza o desperdício ao reciclar e reutilizar
-                sempre que possível.
-              </p>
-            </div>
-
-            <div
-              className="col-md-3 col-sm-12"
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.363)",
-                borderRadius: "15px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img className={styles["img-inter"]} src={icon} alt="" />
-              <div className={styles["texto-sobre"]}>Porque?</div>
-              <p className={styles["consumo"]}>
-                Adotar o consumo consciente preserva recursos naturais e
-                contribui para um meio ambiente mais saudável. Além disso,
-                promove uma sociedade mais equitativa, apoiando práticas
-                comerciais éticas e sustentáveis.
-              </p>
-
-              
-            </div>
-            
-
-            <section>
-              <div
-                className="container-poluição"
-                style={{ display: "flex", alignItems: "flex-start" }}
-              >
-               
-                <h1 className={styles["poluicao"]}>POLUIÇÃO!</h1>
-                <p className={styles["poluicao-texto"]}>
-                  Nos mares do nosso planeta, um drama silencioso se desenrola
-                  diariamente. <br />
-                  Milhões de toneladas de plástico invadem nossos oceanos,
-                  transformando a <br />
-                  majestosa vastidão azul em um trágico cenário de poluição. A
-                  vida marinha,
-                  <br /> antes vibrante, agora luta pela sobrevivência em um mar
-                  de detritos
-                  <br /> plásticos. É hora de mudar esse enredo. Juntos, podemos
-                  recusar o uso de <br />
-                  plástico descartável, abraçar alternativas sustentáveis e
-                  restaurar a beleza <br />
-                  dos oceanos que são a essência da vida na Terra. O futuro do
-                  mar está em
-                  <br /> nossas mãos. #PareComOPlástico, #SalveOsOceanos.
-                </p>
-                </div>
-                <div className="row">
-               <div className="col-sm-12 col-md-6 col-lg-4 offset-md-1 mb-1 ">
-                <img
-                className={`${styles["img-tartaruga"]} img-fluid mx auto`}
-               src={tartaruga}
-                alt=""
-               style={{
-                display: "flex",
-                flexDirection: "column",
-                width: 370,
-                height: 260,
-                borderRadius: "15px",
-                marginTop: "50px",
-                
-                
-               }}
-              />
-            </div>
-
-            <div className="col-sm-12 col-md-6 col-lg-4 ">
-             <img
-              className={`${styles["img-foca"]} img-fluid mx-auto`}
-               src={foca}
-               alt=""
-              style={{
-               display: "flex",
-               flexDirection: "column",
-               width: 370,
-               height: 260,
-               borderRadius: "15px",
-               marginTop: "50px",
-              
-              }}
-              />
+          className={` ${styles["caixa-sobre"]} ${styles["consumo-box"]}`}
+          >
+          <img className={styles["img-inter"]} src={icon} alt="" />
+         <div className={styles["texto-sobre"]}>O que?</div>
+           <p className={styles["consumo"]}>
+          Consumo consciente é fazer escolhas de compra considerando o
+          impacto ambiental e social dos produtos. Envolve preferir opções
+          sustentáveis e éticas para promover um estilo de vida mais
+          responsável.
+         </p>
+        </div>
+        </div>
+         <div className="col-sm-12 col-md-4 mb-4">
+         
+          <div
+         className={` ${styles["caixa-sobre"]} ${styles["consumo-box"]} ` }
+         >
+        <img className={styles["img-inter"]} src={icon} alt="" />
+        <div className={styles["texto-sobre"]}>Como?</div>
+        <p className={styles["consumo"]}>
+          Pratique o consumo consciente ao optar por produtos duráveis e
+          sustentáveis. Reduza o desperdício ao reciclar e reutilizar
+          sempre que possível.
+        </p>
            </div>
-          </div>
+         </div>
+         <div className="col-sm-12 col-md-4 mb-4">
+         <div
+          className={` ${styles["caixa-sobre"]} ${styles["consumo-box"]}`}
+        >
+        <img className={styles["img-inter"]} src={icon} alt="" />
+        <div className={styles["texto-sobre"]}>Porque?</div>
+        <p className={styles["consumo"]}>
+          Adotar o consumo consciente preserva recursos naturais e
+          contribui para um meio ambiente mais saudável. Além disso,
+          promove uma sociedade mais equitativa, apoiando práticas
+          comerciais éticas e sustentáveis.
+        </p>
+        </div>
+       </div>
+      </div>
+      
 
-           
-              
-                
-            </section>
+      <div className="container-poluição mt-5">
+       <h1 className={styles["poluicao"]}>POLUIÇÃO!</h1>
+        <p className={styles["poluicao-texto"]}>
+        Nos mares do nosso planeta, um drama silencioso se desenrola
+        diariamente. <br />
+        Milhões de toneladas de plástico invadem nossos oceanos,
+        transformando a <br />
+        majestosa vastidão azul em um trágico cenário de poluição. A
+        vida marinha,
+        <br /> antes vibrante, agora luta pela sobrevivência em um mar
+        de detritos
+        <br /> plásticos. É hora de mudar esse enredo. Juntos, podemos
+        recusar o uso de <br />
+        plástico descartável, abraçar alternativas sustentáveis e
+        restaurar a beleza <br />
+        dos oceanos que são a essência da vida na Terra. O futuro do
+        mar está em
+        <br /> nossas mãos. #PareComOPlástico, #SalveOsOceanos.
+      </p>
+    </div>
+
+       <div className="row mt-5">
+       <div className="col-sm-12 col-md-6 col-lg-4 mb-4 offset-md-1">
+        <img
+        className={`${styles["img-tartaruga"]} img-fluid`}
+        src={tartaruga}
+        alt=""
+        style={{
+          width: "93%",
+          height: "auto",
+          borderRadius: "15px",
+          right:100,
+        }}
+      />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+      <img
+        className={`${styles["img-foca"]} img-fluid`}
+        src={foca}
+        alt=""
+        style={{
+          width: "90%",
+          height: "auto",
+          borderRadius: "15px",
+          
+          
+        }}
+       />
+      </div>
+    </div>
+  
+  </section>
+  
+</section>
+
 
             <section>
+              
               <div
-                className="container-pesca"
+                className="container-pesca mt-5"
                 style={{ display: "flex", alignItems: "flex-start" }}
-              >
+              > 
                 <h1
                   className={styles["pesca-texto"]}
-                  style={{ whiteSpace: "nowrap" }}
+                  style={{ whiteSpace: "nowrap" , textAlign: "right", marginRight: "200px",}}
+                  
                 >
                   PESCA PROIBIDA!
                 </h1>
@@ -197,7 +158,7 @@ const Consumo = () => {
 
                 <div className="container mt-4 ">
                   <div className="row">
-                    <div className="col-md-4 col-sm-12  ">
+                    <div className="col-sm-12 col-md-4 mb-4">
                       <div
                         className="card"
                         style={{
@@ -206,10 +167,10 @@ const Consumo = () => {
                           display: "flex",
                           flexDirection: "column",
                           alignItems: "center",
-                          right: -30,
                           top: "30%",
                           width: 370,
                           height: 400,
+                          left:100
                         }}
                       >
                         <div className="card-body d-flex flex-column align-items-start">
@@ -259,7 +220,6 @@ const Consumo = () => {
                   </h1>
                   <div className="lixo">
                     <img className={styles["img-lixo"]} src={lixo} alt=""></img>
-                    
 
                     <h1
                       className={styles["lixo-texto"]}
@@ -341,9 +301,7 @@ const Consumo = () => {
               </div>
             </section>
           </div>
-        </section>
-      </section>
-    </div>
+      
   );
 };
 export { Consumo };
